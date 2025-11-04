a7b34c5c212528743515771cf862dce26fa53f80
vijay-cmtai <vijay.maurya@completrix.com>
2025-11-04 12:51:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,12 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "./", // ✅ important for Vercel
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -20,7 +18,7 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: "dist",
     rollupOptions: {
-      input: "./index.html", 
+      input: "./index.html",
     },
   },
 }));
