91ad8c0e20ac62ed39b7eb9e9138a0b2961063b9
vijay-cmtai <vijay.maurya@completrix.com>
2025-11-04 12:56:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "./", // ✅ relative path important for Vercel
+  base: "./", // ✅ relative paths for deployment (important for Vercel)
   server: {
     host: "::",
     port: 8080,
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: "dist",
     rollupOptions: {
-      // ✅ use absolute path so Rollup can find it properly
+      // ✅ use absolute path so Rollup can find index.html properly
       input: path.resolve(__dirname, "index.html"),
     },
   },
