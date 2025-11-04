22bb7790fb9ee55cc88c941064c12558b851ff85
vijay-cmtai <vijay.maurya@completrix.com>
2025-11-04 11:33:06 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: import.meta.env.VITE_BACKEND_URL + "/api/v1",
 });
 
 API.interceptors.request.use((req) => {
